cf9c73d1d47f9501bafa330f9f4d822518751e1d
davis <kuangzt_e@163.com>
2017-12-16 22:52:05 +0800

    拨打页面自动串联

--- app/src/main/java/com/wechataccessibility/MainActivity.java
+++ app/src/main/java/com/wechataccessibility/MainActivity.java
@@ -1,8 +1,17 @@
 package com.wechataccessibility;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.text.TextUtilsCompat;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * @author
@@ -10,11 +19,40 @@ import android.support.annotation.Nullable;
  * @date 2017/12/11 22:49
  * @copyright
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
+    EditText mNameEt;
+    Button mOkBt;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        init();
+    }
+
+    private void init() {
+        mNameEt = (EditText) findViewById(R.id.name);
+        mOkBt = (Button) findViewById(R.id.ok);
+        mOkBt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.ok:
+                if (TextUtils.isEmpty(mNameEt.getText())) {
+                    Toast.makeText(this,R.string.name_cannot_null,Toast.LENGTH_LONG).show();
+                } else {
+                    SharedPreferences sp = getSharedPreferences("app", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("chatname",mNameEt.getText().toString());
+                    editor.commit();
+                    Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.tencent.mm");
+                    startActivity(LaunchIntent);
+
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/wechataccessibility/WeChatAccessibility.java
+++ app/src/main/java/com/wechataccessibility/WeChatAccessibility.java
@@ -2,7 +2,10 @@ package com.wechataccessibility;
 
 import android.accessibilityservice.AccessibilityService;
 import android.app.Notification;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -35,9 +38,45 @@ public class WeChatAccessibility extends AccessibilityService {
                 //notificationEvent(text, (Notification) data);
             }
         } else if(eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+
+            String itemNameTvId = "com.tencent.mm:id/aoj";//微信首页聊天记录
+            AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+            List<AccessibilityNodeInfo> datas = nodeInfo.findAccessibilityNodeInfosByViewId(itemNameTvId);
+            if (datas != null) {
+                SharedPreferences sp = getSharedPreferences("app", Context.MODE_PRIVATE);
+                String chatName = sp.getString("chatname","");
+                for(AccessibilityNodeInfo data:datas) {
+                    if (chatName.equals(data.getText().toString())) {
+                        AccessibilityHelper.performClick(data);
+                    }
+                }
+            }
+
+            String plusId = "com.tencent.mm:id/aa6";//聊天页面的加号
+            AccessibilityNodeInfo plusNode = AccessibilityHelper.findNodeInfosById(nodeInfo,plusId);
+            if (plusNode != null) {
+                AccessibilityHelper.performClick(plusNode);
+            }
+
+            String o4 = "com.tencent.mm:id/o4";//聊天页面点击加号后，各功能项的图标
+            List<AccessibilityNodeInfo> icons = nodeInfo.findAccessibilityNodeInfosByViewId(o4);
+            if (icons != null) {
+                if (icons.size() > 2 ) {
+                    AccessibilityHelper.performClick(icons.get(2));
+                }
+            }
+
+            String fy = "com.tencent.mm:id/fy";//对话框上的"视频聊天""语音聊天"
+            List<AccessibilityNodeInfo> fys = nodeInfo.findAccessibilityNodeInfosByViewId(fy);
+            if (fys != null) {
+                if (fys.size() > 0 ) {
+                    AccessibilityHelper.performClick(fys.get(0));
+                }
+            }
+
+            //接听页面
             String layoutId = "com.tencent.mm:id/dcs";
             String buttonId = "com.tencent.mm:id/bps";
-            AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
             AccessibilityNodeInfo layoutNode = AccessibilityHelper.findNodeInfosById(nodeInfo, layoutId);
             if (layoutNode != null) {
                 AccessibilityNodeInfo targetNode = AccessibilityHelper.findNodeInfosById(layoutNode, buttonId);
@@ -46,10 +85,12 @@ public class WeChatAccessibility extends AccessibilityService {
                 }
             }
 
+            nodeInfo.recycle();
             //openHongBao(event);
         } else if(eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
 
         }
+
     }
 
     @Override
